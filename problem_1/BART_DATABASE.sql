c88b4b3923318cfe8c2ca7cf030006728dd98a34
Taylor Yang <rainbowww5@gmail.com>
2020-02-27 16:56:52 -0800

    update .sql file

--- problem_1/BART_DATABASE.sql
+++ problem_1/BART_DATABASE.sql
@@ -23,9 +23,10 @@ DROP TABLE IF EXISTS `T_BART_STATION`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `T_BART_STATION` (
-  `ID` int(11) DEFAULT NULL,
-  `NAME` varchar(32) DEFAULT NULL,
-  `CODE` varchar(4) DEFAULT NULL
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(64) DEFAULT NULL,
+  `CODE` varchar(4) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -46,12 +47,13 @@ DROP TABLE IF EXISTS `T_RIDERSHIP`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `T_RIDERSHIP` (
-  `ID` int(11) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `MONTH` int(11) DEFAULT NULL,
   `YEAR` int(11) DEFAULT NULL,
   `ENTRY_STATION_ID` int(11) DEFAULT NULL,
   `EXIT_STATION_ID` int(11) DEFAULT NULL,
-  `COUNT` int(11) DEFAULT NULL
+  `COUNT` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -73,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-23 16:59:27
+-- Dump completed on 2020-02-27 16:56:33
